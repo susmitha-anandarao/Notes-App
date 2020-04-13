ecd98c3294172d67f38224dc28900feb2485f73c
Susmitha Anandarao <susmitha@ChaiOne-Standard-065.local>
2020-04-12 21:04:47 -0500

    Remove unusd method

--- notes.js
+++ notes.js
@@ -1,10 +1,6 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-const getNotes = () => {
-    return 'Your notes...'
-}
-
 const addNote = (title, body) => {
 
     const notes = loadNotes()
@@ -75,7 +71,6 @@ const loadNotes = () => {
 }
 
 module.exports = {
-    getNotes: getNotes,
     addNote: addNote,
     removeNote: removeNote,
     listNotes: listNotes,
